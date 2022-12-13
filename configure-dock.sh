a87eae1f53a60bfdc55f25c6ebddd5712f6d4130
mathijs.w <mathijs@prowarehouse.nl>
2022-12-13 23:25:30 +0100

    Supports macOS Ventura

--- configure-dock.sh
+++ configure-dock.sh
@@ -2,79 +2,106 @@
 
 # SCRIPT USES DOCKUTILv3
 # https://github.com/kcrawford/dockutil
-
-#Folders to add to the dock
-user_folders=("Downloads")
-
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Variables
+# VARIABLES
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 macosBuild=$(sw_vers -buildVersion)
 macosProduct=$(sw_vers -productVersion)
 macosShortVers=$(echo "$macosBuild" | head -c2)
 dockutil=$(which dockutil)
 sleepTime=5
 
-echo "Removing all current dock items"
-$dockutil --remove all --no-restart --allhomes
-
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Functions for different macOS builds
+# FUNCTIONS
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-function launchpad_appstore() {
-	# IF CATALINA OR HIGHER
+function launchpad() {
+	# IF MACOS 10.15 OR HIGHER
 	if [ "$macosShortVers" -ge 19 ] ; then
-		# ADD LAUNCHPAD ON CATALINE OR HIGHER
-		echo "macOS version: $macosProduct, will use path /System/Applications/"
-		$dockutil --add "/System/Applications/Launchpad.app" --no-restart --allhomes
-		$dockutil --add "/System/Applications/App Store.app" --no-restart --allhomes
+		path="/System/Applications"
 	else
 		# ADD LAUNCHPAD ON LEGACY OS
-		echo "macOS version: $macosProduct, will path /Applications/"
-		$dockutil --add "/Applications/Launchpad.app" --no-restart --allhomes
-		$dockutil --add "/Applications/App Store.app" --no-restart --allhomes	
+		path="/Applications"
 	fi
+	launchpath="${path}/Launchpad.app"
 }
+launchpad
 
-function system_setting() {
-	# SYSTEM SETTINGS / SYSTEM PREFERENCES
-	if [ "$macosShortVers" -ge 22 ]; then
-		# ADD SYSTEM SETTINGS ON VENTURA OR HIHGER
-		echo "macOS version: $macosProduct, will use System Settings.app"
-		$dockutil --add "/System/Applications/System Settings.app" --no-restart --allhomes
+function appstore() {
+	# IF MACOS 10.15 OR HIGHER
+	if [ "$macosShortVers" -ge 19 ] ; then
+		path="/System/Applications"
 	else
-		# ADD SYSTEM PREFERENCES ON MONTEREY AND LOWER
-		echo "macOS version: $macosProduct, will use System Preferences.app"
-		$dockutil --add "/System/Applications/System Preferences.app" --no-restart --allhomes
+		# ADD LAUNCHPAD ON LEGACY OS
+		path="/Applications"
 	fi
+	appstorepath="${path}/App Store.app"
 }
+appstore
+
+function system_settings() {
+	if [ "$macosShortVers" -ge 22 ]; then
+		# IF MACOS 13 OR HIHGER
+		path="/System/Applications/System Settings.app"
+	else
+		# IF MACOS 12 OR LOWER
+		path="/System/Applications/System Preferences.app"
+	fi
+	systempath=${path}
+}
+system_settings
+
+function safari() {
+	if [ "$macosShortVers" -ge 22 ]; then
+		# IF MACOS 13 OR HIHGER
+		path="/System/Cryptexes/App/System/Applications"
+	else
+		# IF MACOS 12 OR LOWER
+		path="/Applications"
+	fi
+	safaripath="${path}/Safari.app"
+}
+safari
 
 function dockutil_default() {
+	# ADD APP(S) IN DOCK
+	for app in "${user_apps[@]}"; do
+		$dockutil --add "${app}" --no-restart --allhomes
+	done
 	
-	launchpad_appstore
-	$dockutil --add "/Applications/Self Service.app" --no-restart --allhomes
-	$dockutil --add "/Applications/Safari.app" --no-restart --allhomes
-	$dockutil --add "/Applications/Google Chrome.app" --no-restart --allhomes
-	$dockutil --add "/Applications/Microsoft Excel.app" --no-restart --allhomes
-	$dockutil --add "/Applications/Microsoft OneNote.app" --no-restart --allhomes
-	$dockutil --add "/Applications/Microsoft Outlook.app" --no-restart --allhomes
-	$dockutil --add "/Applications/Microsoft PowerPoint.app" --no-restart --allhomes
-	$dockutil --add "/Applications/Microsoft Word.app" --no-restart --allhomes
-	$dockutil --add "/Applications/Microsoft Teams.app" --no-restart --allhomes
-	system_setting
-	$dockutil --add "/Applications/TeamViewerQS.app" --no-restart --allhomes
-	
+	# ADD FOLDER(S) IN DOCK
 	for folder in "${user_folders[@]}"; do
 		$dockutil --add "~/${folder}" --no-restart --allhomes
 	done
 }
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# APP LIST
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+user_apps=(
+	"$launchpath"  ### LAUNCHPAD
+	"$appstorepath" ### APPSTORE
+	"/Applications/Privileges.app"
+	"/Applications/Self Service.app"
+	"$safaripath" ### SAFARI
+	"/Applications/Microsoft Outlook.app"
+	"/Applications/Microsoft Word.app"
+	"/Applications/Microsoft Excel.app"
+	"/Applications/Microsoft PowerPoint.app"
+	"/Applications/Microsoft Teams.app"
+	"/Applications/Visual Studio Code.app"
+	"$systempath" ### SYSTEM SETTINGS
+)
+
+user_folders=(
+	"Downloads"  ### DOWNLOADS FOLDER
+)
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Start script
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
 
 dockutil_default
 
@@ -86,5 +113,3 @@ sleep $sleepTime
 
 echo "Restarting Dock"
 /usr/bin/killall Dock >/dev/null 2>&1
-
-exit 0
